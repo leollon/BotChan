ece31466238f3659ede2396c136c9bc3b42528ef
leollon <ccsapp.tux@gmail.com>
2020-01-05 22:36:31 +0800

    add:celery:add celery worker monitor
    use shell script to monitoring ssh login

---
+++ ServerChan/svrbot/monitor/celery_worker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# goal: monitor celery workers status if it is online.
+# location: crontab
+# mode: change mode to 555
+
+tg_bot_token=your-own-bot-token
+chat_id=your-own-chat-id
+flower_dashboard_url="127.0.0.1:5555/dashboard?json=1"
+http_basic_auth=username:password
+
+message=$(curl -fsL -H WWW-Authentication=${http_basic_auth} "${flower_dashboard_url}" -o - | jq . - | grep -E "status|hostname" | tr -d " ,")
+curl -s "https://api.telegram.org/bot${tg_bot_token}/sendMessage?chat_id=${chat_id}" --data-binary "&text=${message}"
+
---
+++ ServerChan/svrbot/monitor/ssh_login.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ambition: monitor ssh login and send message to telegram.
+# location: copy to /etc/profile.d/
+# mode: change mode to 555
+
+tg_bot_token=your-own-bot-token
+chat_id=your-own-chat-id
+
+message=$(hostanme && TZ=UTC-8 date && who && w | awk 'BEGIN{OFS="\t"}{print $1,$8}')
+curl -s "https://api.telegram.org/bot${tg_bot_token}/sendMessage?chat_id=${chat_id}" --data-binary "&text=${message}"
